730d952297d13100f4773b6f0f6fbe10273084c9
Karina Campos <2109020@upy.edu.mx>
2022-05-17 21:44:23 -0500

    time example

---
+++ ifTime.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	
+	int time = 12;
+	if (time < 18){
+		cout << "Good day.";
+		if (time == 12){
+			cout<< "It's noon";
+		}
+	}else {
+			cout << "Good evening";
+		
+	}
+}
